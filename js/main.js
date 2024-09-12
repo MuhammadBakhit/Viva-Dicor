b43407af3904695195c4284bf87def7a9ecac634
Muhammad Bekhit <mohamedbakhetoiu@gmail.com>
2024-09-12 19:45:31 +0300

    Viva-Dicor

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// let span = document.querySelector(".up");
+
+// window.onscroll = function () {
+//     this.scrollY >= 60 ?  span.classList.add("show") : span.classList.remove("show");
+// } 
+
+// span.onclick =function click() {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//     });
+// };
